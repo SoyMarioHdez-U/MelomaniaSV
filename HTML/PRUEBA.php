195df8ec6aa3842e170be4bc71a346f263ac2be5
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2023-10-01 18:47:59 -0600

    hola

---
+++ HTML/PRUEBA.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="dropdownmenu">
+  <ul>
+    <li><a href="http://www.jochaho.com/wordpress/">Home</a></li>
+    <li><a href="http://www.jochaho.com/wordpress/about-pritesh-badge/">About Me</a></li>
+    <li><a href="#">Articles on HTML5 & CSS3</a>
+      <ul id="submenu">
+        <li><a href="http://www.jochaho.com/wordpress/difference-between-svg-vs-canvas/">Difference between SVG vs. Canvas</a></li>
+        <li><a href="http://www.jochaho.com/wordpress/new-features-in-html5/">New features in HTML5</a></li>
+        <li><a href="http://www.jochaho.com/wordpress/creating-links-to-sections-within-a-webpage/">Creating links to sections within a webpage</a></li>
+      </ul>
+    </li>
+    <li><a href="http://www.jochaho.com/wordpress/category/news/">News</a></li>
+    <li><a href="http://www.jochaho.com/wordpress/about-pritesh-badge/">Contact Us</a></li>
+  </ul>
+</nav>
+</body>
+</html>
