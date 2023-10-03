5f5c34bc947a5618d581c240824c789239b294a6
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2023-10-03 13:18:18 -0600

    Hay un sólo desmadre...

--- HTML/index.php
+++ HTML/index.php
@@ -1,80 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title class="title">Melomanía SV - Bienvenido</title>
-    <link rel="stylesheet" href="http://localhost/melomania/MelomaniaSV/CSS/style.css"> 
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido - MelomaniaSV</title>
 
-<body class="fondo">
-
-    <table>
-        <tr class="header">
-            <td colspan="4" align="left"><a href="http://localhost/melomania/MelomaniaSV/HTML/index.php"><img src="/Img/Logo grande.png" height="50px"></a></td>
-            <td colspan="3"><a class="urlUsr" href="http://localhost/melomania/MelomaniaSV/HTML/iniciarsesion.php">Iniciar Sesión</a></td>
-            
-            <td class="urlUsr"><a class="urlUsr" href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php">Registrarse</a></td>
-            <td class="urlUsr">Carrito </td>
-        </tr>
-        <tr class="menu">
-            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/index.php">Inicio</a></td>
-            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php">Tienda</a></td>
-            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/cursos.php">Cursos</a></td>
-            <td><a class="urlIni">Contactos</a></td>
-            <td colspan="6"></td>
-            
-        </tr>
-        <tr class="secciones">
-            <td colspan="10"><h1 class="titles">¡Bienvenido a Melomanía!</h1>
-            <p class="titleContent">Somos un negocio dedicado a la venta de <br>
-                instrumentos musicales y equipos de sonido. <br>
-                Ofrecemos un gran abanico de marcas <br>
-                internacionales y cursos educativos para <br>
-                el instrumento de tu preferenciaaaaaaaaaaaaaaaa.</p></td>
-        </tr>
-        <tr class="secciones">
-            <td colspan="10"><h2 class="titles">Te ofrecemos:</h2></td>
-        </tr>
-        
-        <tr class="secciones">
-            <td colspan="10"><h1 class="titles">Cursos</h1>
-            <p class="titleContent">Te enseñamos a ejecutar el instrumento de tu preferencia <br> 
-                o perfeccionamos tu técnica. <br>
-                Tenemos cursos disponibles para todo tipo de músicos, <br>
-                desde principiante hasta los más avanzados.</p>
-                <a href="http://localhost/melomania/MelomaniaSV/HTML/cursos.php"><button>Ir a los cursos</button></a>
-                <p></p>
-                <p></p>
-            </td>
-        </tr>
-        
-        <tr class="secciones">
-            <td colspan="10"><h1 class="titles">Tienda</h1>
-            <p class="titleContent">Te ofrecemos instrumentos musicales<br> y equipo de sonido de las marcas más reconocidas.</p>
-                <a href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php"><button>Ir a la Tienda</button></a>
-                <p></p>
-                <p></p>
-            </td>
-        </tr>
-                
-        <tr class="footer">
-            <td colspan="10">
-                <p></p>
-                <p></p>
-                <img src="/Img/Logo grande.png" height="50px"><br>
-                <img src="https://toppng.com/uploads/thumbnail/iconos-facebook-icono-de-facebook-twitter-png-y-psd-iconos-redes-sociales-11562849003jm0njbtfm3.png" height="50px"><br>
-                <p>© 2023 Melomanía. Universidad Católica de El Salvador.</p>
-            </td>
-        </tr>
-        
-        
-        
-        
-    </table>
+    <!-- Estilos de css y bootstrap -->
+    <link rel="stylesheet" href="http://localhost/MelomaniaSV/CSS/style.css"> 
     
-
+    
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo"><a href="http://localhost//MelomaniaSV/HTML/"><img src="http://localhost/MelomaniaSV/Img/Logo.png" height="50px"></a></div>
+            <div class="sesion">
+                <div class="ini-sesion"><a href="http://localhost//MelomaniaSV/HTML/iniciarsesion.php">INICIAR SESIÓN</a></div>
+                <div class="registro">REGÍSTRATE</div>
+                <div class="carrito">$0.00</div>
+            </div>
+            
+        </div>
+        <div class="nav">
+            <div><a href="http://localhost//MelomaniaSV/HTML/">Inicio</a></div>
+        </div>
+        <div class="content">
+            content
+        </div>
+        <div class="footer">
+            footer
+        </div>
+    </div>
+    
 </body>
-
 </html>
+
+
+
+
+
+
+
+
+
---
+++ HTML/inicio.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title class="title">Melomanía SV - Bienvenido</title>
+    <link rel="stylesheet" href="http://localhost/MelomaniaSV/CSS/style.css"> 
+    <link rel="stylesheet" href="http://localhost/MelomaniaSV/CSS/respaldo.css"> 
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</head>
+
+<body class="fondo">
+
+    <table>
+        <tr class="header">
+            <td colspan="4" align="left"><a href="http://localhost//MelomaniaSV/HTML/"><img src="/Img/Logo grande.png" height="50px"></a></td>
+            <td colspan="3"><a class="urlUsr" href="http://localhost/melomania/MelomaniaSV/HTML/iniciarsesion.php">Iniciar Sesión</a></td>
+            
+            <td class="urlUsr"><a class="urlUsr" href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php">Registrarse</a></td>
+            <td class="urlUsr">Carrito </td>
+        </tr>
+        <tr class="menu">
+            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/index.php">Inicio</a></td>
+            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php">Tienda</a></td>
+            <td><a class="urlIni" href="http://localhost/melomania/MelomaniaSV/HTML/cursos.php">Cursos</a></td>
+            <td><a class="urlIni">Contactos</a></td>
+            <td colspan="6"></td>
+            
+        </tr>
+        <tr class="secciones">
+            <td colspan="10"><h1 class="titles">¡Bienvenido a Melomanía!</h1>
+            <p class="titleContent">Somos un negocio dedicado a la venta de <br>
+                instrumentos musicales y equipos de sonido. <br>
+                Ofrecemos un gran abanico de marcas <br>
+                internacionales y cursos educativos para <br>
+                el instrumento de tu preferenciaaaaaaaaaaaaaaaa.</p></td>
+        </tr>
+        <tr class="secciones">
+            <td colspan="10"><h2 class="titles">Te ofrecemos:</h2></td>
+        </tr>
+        
+        <tr class="secciones">
+            <td colspan="10"><h1 class="titles">Cursos</h1>
+            <p class="titleContent">Te enseñamos a ejecutar el instrumento de tu preferencia <br> 
+                o perfeccionamos tu técnica. <br>
+                Tenemos cursos disponibles para todo tipo de músicos, <br>
+                desde principiante hasta los más avanzados.</p>
+                <a href="http://localhost/melomania/MelomaniaSV/HTML/cursos.php"><button>Ir a los cursos</button></a>
+                <p></p>
+                <p></p>
+            </td>
+        </tr>
+        
+        <tr class="secciones">
+            <td colspan="10"><h1 class="titles">Tienda</h1>
+            <p class="titleContent">Te ofrecemos instrumentos musicales<br> y equipo de sonido de las marcas más reconocidas.</p>
+                <a href="http://localhost/melomania/MelomaniaSV/HTML/tienda.php"><button>Ir a la Tienda</button></a>
+                <p></p>
+                <p></p>
+            </td>
+        </tr>
+                
+        <tr class="footer">
+            <td colspan="10">
+                <p></p>
+                <p></p>
+                <img src="/Img/Logo grande.png" height="50px"><br>
+                <img src="https://toppng.com/uploads/thumbnail/iconos-facebook-icono-de-facebook-twitter-png-y-psd-iconos-redes-sociales-11562849003jm0njbtfm3.png" height="50px"><br>
+                <p>© 2023 Melomanía. Universidad Católica de El Salvador.</p>
+            </td>
+        </tr>
+        
+        
+        
+        
+    </table>
+    
+
+</body>
+
+</html>
