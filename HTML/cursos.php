3e61cb68366409a667df7f2851b6cbaac7213e46
alerodriguez23 <jenniffer.rodriguez@catolica.edu.sv>
2023-10-03 21:58:18 -0600

    rutas

--- HTML/cursos.php
+++ HTML/cursos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/MelomaniaSV/CSS/style.css"> 
+    <link rel="stylesheet" href="https://localhost/MelomaniaSV/CSS/style.css"> 
 
 </head>
 <body class="fondo">
@@ -15,7 +15,7 @@
   <!-- Barra superior de inicio y menú -->  
   <table>
           <tr class="header">
-              <td colspan="4" align="left"><a href="http://localhost//MelomaniaSV/HTML/"><img src="/Img/Logo grande.png" height="50px"></a></td>
+              <td colspan="4" align="left"><a href="http://localhost/MelomaniaSV/HTML/"><img src="/Img/Logo grande.png" height="50px"></a></td>
               <td colspan="3"><a class="urlUsr" href="http://localhost/MelomaniaSV/HTML/iniciarsesion.php">Iniciar Sesión</a></td>
               
               <td class="urlUsr"><a class="urlUsr" href="http://localhost/MelomaniaSV/HTML/tienda.php">Registrarse</a></td>
